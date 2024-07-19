a8c6d030881e7f11b0afc60b264f77c7bb7c6920
David Mutchler <mutchler@rose-hulman.edu>
2024-07-19 15:45:26 -0400

    Various small improvements/changes made when I recorded the Follow-Me video.

--- solution (no peeking!)/Ball.py
+++ solution (no peeking!)/Ball.py
@@ -4,38 +4,35 @@ import random
 
 class Ball:
     def __init__(self, screen: pygame.Surface,
-                 min_radius=5, max_radius=20,
-                 min_speed=1, max_speed=5):
+                 min_radius=10, max_radius=40,
+                 min_speed=1, max_speed=8):
         self.screen = screen
-
-        # Random position, color and (within specified range) radius:
+        self.color = (random.randint(0, 255),
+                      random.randint(0, 255),
+                      random.randint(0, 255))
         self.x = random.randint(0, self.screen.get_width() - 1)
         self.y = random.randint(0, self.screen.get_height() - 1)
-        self.color = pygame.Color((random.randint(0, 255),
-                                   random.randint(0, 255),
-                                   random.randint(0, 255)))
         self.radius = random.randint(min_radius, max_radius)
 
-        # Random speeds in x/y directions within specified range:
-        x_direction = random.choice((1, -1))
+        direction = random.choice((-1, 1))
         x_above_min = random.random() * (max_speed - min_speed)
-        self.speed_x = x_direction * (min_speed + x_above_min)
+        self.speed_x = direction * x_above_min
 
-        y_direction = random.choice((1, -1))
+        direction = random.choice((-1, 1))
         y_above_min = random.random() * (max_speed - min_speed)
-        self.speed_y = y_direction * (min_speed + y_above_min)
+        self.speed_y = direction * y_above_min
 
     def move(self):
-        self.x = self.x + self.speed_x
-        self.y = self.y + self.speed_y
-        self.bounce_if_past_edge()
+        self.x += self.speed_x
+        self.y += self.speed_y
+        self.bounce_if_offscreen()
 
-    def bounce_if_past_edge(self):
+    def bounce_if_offscreen(self):
         if self.x < 0 or self.x >= self.screen.get_width():
             self.speed_x = -self.speed_x
         if self.y < 0 or self.y >= self.screen.get_height():
             self.speed_y = -self.speed_y
 
     def draw(self):
-        pygame.draw.circle(self.screen, self.color, (self.x, self.y),
-                           self.radius)
+        pygame.draw.circle(self.screen, self.color,
+                           (self.x, self.y), self.radius)
--- solution (no peeking!)/Balls.py
+++ solution (no peeking!)/Balls.py
@@ -1,19 +1,23 @@
+import pygame
 from Ball import Ball
 
 
 class Balls:
-    def __init__(self, screen, number_of_balls,
-                 min_radius=5, max_radius=20,
-                 min_speed=1, max_speed=5):
-        self.balls = []
+    def __init__(self, screen: pygame.Surface,
+                 number_of_balls,
+                 min_radius=10, max_radius=40,
+                 min_speed=1, max_speed=8):
+        self.list_of_balls = []
         for k in range(number_of_balls):
-            ball = Ball(screen, min_radius, max_radius, min_speed, max_speed)
-            self.balls.append(ball)
+            ball = Ball(screen,
+                        min_radius, max_radius,
+                        min_speed, max_speed)
+            self.list_of_balls.append(ball)
 
     def move(self):
-        for ball in self.balls:
+        for ball in self.list_of_balls:
             ball.move()
 
     def draw(self):
-        for ball in self.balls:
+        for ball in self.list_of_balls:
             ball.draw()
